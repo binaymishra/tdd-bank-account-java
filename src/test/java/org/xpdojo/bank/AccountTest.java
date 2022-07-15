9698b20408a0475453dbc5bc82c7e2cce0dc2482
Binay Mishra <binaymishramca@gmail.com>
2022-07-15 17:42:40 +0530

    First simple test

--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -1,5 +1,6 @@
 package org.xpdojo.bank;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
@@ -7,9 +8,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class AccountTest {
 
-    @Test
-	@Disabled
-    public void depositAnAmountToIncreaseTheBalance() {
-        assertThat("your first test").isBlank();
-    }
+  @Test
+  @Disabled
+  public void depositAnAmountToIncreaseTheBalance() {
+    assertThat("your first test").isBlank();
+  }
+
+  @Test
+  public void isAccountExists() {
+    Account account = new Account();
+    Assertions.assertNotNull(account);
+    // Compilation is good Account object does exists and successfully created asserted notNull
+  }
 }
